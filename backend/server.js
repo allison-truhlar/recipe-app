f9c480000c55dcd8c8b8e3401b3cac4cff367158
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-18 14:06:25 -0400

    Update server.js
    Added userRoutes to the server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 const express = require("express")
 const mongoose = require("mongoose")
 const recipeRoutes = require("./routes/recipes")
+const userRoutes = require("./routes/users")
 
 // Create express app
 const app = express()
@@ -17,6 +18,7 @@ app.use((req,res,next)=>{
 
 // Routes
 app.use("/api/recipes", recipeRoutes)
+app.use("/api/user", userRoutes)
 
 // Connect to DB
 mongoose.connect((process.env.MONGO_URI))
