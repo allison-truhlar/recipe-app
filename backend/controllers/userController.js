5d75c22b618c56ff3d564e05026a2f77f58fec7b
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-18 14:06:13 -0400

    Create userController.js
    Added test controllers for user login and signup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+
+
+//login user
+async function loginUser(req, res){
+    res.json({msg: "login user"})
+}
+
+//sign up user
+async function createUser(req, res){
+    res.json({msg: "sign-up user"})
+}
+
+module.exports = {
+    loginUser,
+    createUser
+}
