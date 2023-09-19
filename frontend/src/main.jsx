a22cbe6b048a0f11dfde30d41144aaf62f2d2eff
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-19 13:31:12 -0400

    Add AuthContextProvider to main.jsx
    Wrap the entire app with AuthContextProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from "./App"
 import {RecipesContextProvider} from './context/RecipeContext'
+import {AuthContextProvider} from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RecipesContextProvider>
-      <App/>
-    </RecipesContextProvider>
+    <AuthContextProvider>
+      <RecipesContextProvider>
+        <App/>
+      </RecipesContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 )
