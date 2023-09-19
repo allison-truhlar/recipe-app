29c5d2510e4d7ff9164a588de1a76d9f990f92ad
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-18 20:35:30 -0400

    Update userController.js
    Added jsonwebtoken, createToken function, and logic to create token when a user successfully signs up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 const User = require("../models/userModel")
+const jwt = require("jsonwebtoken")
 const mongoose = require("mongoose")
+
+function createToken(_id){
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: "3d"})
+}
 
 //login user
 async function loginUser(req, res){
@@ -12,7 +17,11 @@ async function createUser(req, res){
 
     try{
         const user = await User.signup(email, password)
-        res.status(200).json({email, user})
+
+        //create token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
 
     } catch(error) {
         res.status(400).json({error: error.message})
