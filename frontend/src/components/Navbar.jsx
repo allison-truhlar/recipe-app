840bc1ca0d3af418045255565f6ddcd905af6ab1
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-19 14:50:00 -0400

    Update Navbar.jsx
    Added the login and sign up links to the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,6 +7,12 @@ export default function Navbar(){
                 <Link to="/">
                     <h1>Recipe Keeper</h1>
                 </Link>
+                <nav>
+                    <div>
+                        <Link to="/login">Log in</Link>
+                        <Link to="/signup">Sign up</Link>
+                    </div>
+                </nav>
             </div>
         </header>
     )
