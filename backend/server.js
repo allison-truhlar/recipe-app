623b8d4dac0b650da556183a61c946149f2fe90d
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-20 16:55:15 -0400

    Update server.js
    Changed to align more closely with the blog post tutorial

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,41 @@
 require("dotenv").config()
 
 const express = require("express")
-const mongoose = require("mongoose")
+const connectDB = require("./config/db")
+
+// Requirements for session tracking
+const cookieParser = require("cookie-parser")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")
+
+// Import routers
 const recipeRoutes = require("./routes/recipes")
 const userRoutes = require("./routes/users")
 
 // Create express app
 const app = express()
-const port = 4000
+const port = process.env.PORT
 
-// Middleware
+// Connect to DB
+connectDB()
+
+// Global middleware
+app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-app.use((req,res,next)=>{
-    // console.log(req.path, req.method)
-    next()
-})
+app.use(cookieParser())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+    })
+}))
 
 // Routes
 app.use("/api/recipes", recipeRoutes)
 app.use("/api/user", userRoutes)
 
-// Connect to DB
-mongoose.connect((process.env.MONGO_URI))
-    .then(() => {
-        // Listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log(`connected to db and server listening on port ${process.env.PORT}`)
-        })
-    })
-    .catch((error) => {
-        console.log(error)
-    })
-
+app.listen(port, () => {
+    console.log(`app listening on port ${process.env.PORT}`)
+})
