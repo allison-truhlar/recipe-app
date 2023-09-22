633d739c0da29fe777722f7effd03577563cbb00
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-22 14:55:43 -0400

    Update recipes.js
    Add origin to the cors middleware (both req and res have to share origin)

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -19,6 +19,7 @@ const router = express.Router()
 // require middleware
 router.use(requireAuth)
 router.use(cors({
+    origin: 'https://master--inquisitive-biscotti-9cbcc6.netlify.app/',
     credentials: true
 }))
 router.use(function (req, res, next) {	 
