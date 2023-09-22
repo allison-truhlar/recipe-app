de90dfa63d12830e098dc001ffb148f8d3465291
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-22 14:24:54 -0400

    Update server.js
    Added cors as middleware, require credentials

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const express = require("express")
+const cors = require("cors")
 const connectDB = require("./config/db")
 
 // Requirements for session tracking
@@ -36,6 +37,9 @@ app.use(session({
 // Other global middleware
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
+app.use(cors({
+    credentials: true
+}))
 
 // Routes
 app.use("/api/recipes", recipeRoutes)
