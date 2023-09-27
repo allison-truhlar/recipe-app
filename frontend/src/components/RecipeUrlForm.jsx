c26c60ead29cd294ade0e52e8a1066ac02aac9ad
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-27 15:31:56 -0400

    Create RecipeUrlForm.jsx
    A new form that allows the user to try to input a new recipe just using an URL

---
+++ frontend/src/components/RecipeUrlForm.jsx
@@ -0,0 +1,59 @@
+import { useState, useContext } from "react"
+import { RecipesContext } from "../context/RecipeContext"
+import { AuthContext } from "../context/AuthContext"
+
+export default function RecipeUrlForm() {
+    const [url, setUrl] = useState("")
+    const [error, setError] = useState(null)
+    const {dispatch} = useContext(RecipesContext)
+    const {user} = useContext(AuthContext)
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault()
+
+        if (!user) {
+            setError("You must be logged in")
+            return
+        }
+
+        const response = await fetch("/api/recipes/url", {
+            method: "POST",
+            body: JSON.stringify({ url }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+
+        const json = await response.json()
+        console.log(response)
+
+        if(!response.ok){
+            setError(json.error)
+        }
+        if(response.ok){
+            setUrl("")
+            setError(null)
+            dispatch({type: "CREATE_RECIPE", payload: json})
+        }
+
+
+    }
+
+    return(
+        <form className="create" onSubmit={handleSubmit}>
+            <h3>Add Recipe with URL</h3>
+            
+            <label>Recipe URL:</label>
+            <input 
+                type="text"
+                onChange={(e) => setUrl(e.target.value)}
+                value={url}
+            />
+
+            <button>Add recipe</button>
+            {error && <div className="error">{error}</div>}
+        </form>
+    )
+}
+
+
