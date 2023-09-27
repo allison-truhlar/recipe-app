eb800533b0a4566008463ba3657504165ea24e97
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-27 15:29:28 -0400

    Update recipeController.js
    Removed logic related to parsing recipe data. This is now housed in a middleware.

--- backend/controllers/recipeController.js
+++ backend/controllers/recipeController.js
@@ -1,7 +1,5 @@
 const Recipe = require("../models/recipeModel")
 const mongoose = require("mongoose")
-const cheerio = require("cheerio")
-const axios = require("axios")
 
 //function to get all recipes
 const getRecipes = async(req, res) =>{
@@ -9,73 +7,38 @@ const getRecipes = async(req, res) =>{
     res.status(200).json(recipes)
 }
 
-//function to create a new recipe from form input
-const createRecipeManual = async(req, res) => {
+//function to create a new recipe 
+const createRecipe = async(req, res) => {
     const {url, name, recipeIngredient, recipeInstructions} = req.body
-    const user_id = req.user._id
+    
+    // if not parsed, check that all required manual input fields are filled
+    if (!req.parsed){
+        let emptyFields = []
 
-    let emptyFields = []
-
-    if (!name){
-        emptyFields.push("name")
+        if (!name) {
+            emptyFields.push("name")
+        }
+        if (!recipeIngredient) {
+            emptyFields.push("recipeIngredient")
+        }
+        if (!recipeInstructions) {
+            emptyFields.push("recipeInstructions")
+        }
+        if (emptyFields.length > 0) {
+            return res.status(400).json({ error: "Please fill in required fields", emptyFields })
+        }
     }
-    if (!recipeIngredient){
-        emptyFields.push("recipeIngredient")
-    }
-    if (!recipeInstructions){
-        emptyFields.push("recipeInstructions")
-    }
-    if(emptyFields.length > 0){
-        return res.status(400).json({error: "Please fill in required fields", emptyFields})
-    }
-
-    addRecipeToDB(url, name, recipeIngredient, recipeInstructions, user_id)
-}
-
-//function to create a new recipe from URL
-const createRecipeUrl = async(req, res) => {
-    const {url} = req.body
-    const user_id = req.user._id
-
-    const response = await axios.get(url);
-    const html = response.data;
-
-    // Use Cheerio to parse the HTML
-    const $ = cheerio.load(html);
-
-    let jsonld;
-    const node = $('script[type="application/ld+json"]').get(0);
-
-    try {
-        jsonld = JSON.parse(node.firstChild.data);
-
-    } catch (err) {
-        // In case of error, you can try to debug by logging the node
-        console.log(node);
-    }
-    const array = jsonld["@graph"]
-    const recipeObj = array.filter(obj => obj['@type'] === 'Recipe')[0];
-    const name = recipeObj.name
-    const recipeIngredient = recipeObj.recipeIngredient
-    const recipeInstructionsText = recipeObj.recipeInstructions.map(instruction => instruction.text);
-
+    
+    //add document to db
     try{
-        const recipe = await Recipe.create({url, name, recipeIngredient, recipeInstructions: recipeInstructionsText, user_id})
-        res.status(200).json(recipe)
-    } catch(error){
-        res.status(400).json({error: error.message})
-    }
-}
-
-//function to add recipe document to DB
-const addRecipeToDB = async (url, name, recipeIngredient, recipeInstructions, user_id) =>{
-    try{
+        const user_id = req.user._id
         const recipe = await Recipe.create({url, name, recipeIngredient, recipeInstructions, user_id})
         res.status(200).json(recipe)
     } catch(error){
         res.status(400).json({error: error.message})
     }
 }
+
 
 //function to delete a recipe
 const deleteRecipe = async(req, res) => {
@@ -95,8 +58,7 @@ const deleteRecipe = async(req, res) => {
 
 module.exports = {
     getRecipes,
-    createRecipeManual,
-    createRecipeUrl,
+    createRecipe,
     deleteRecipe
 }
 
