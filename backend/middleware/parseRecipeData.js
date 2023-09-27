8d00ab6f650f21151077d7ccc23966ed376801d0
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-27 15:28:47 -0400

    Create parseRecipeData.js
    Added middleware to attempt to parse the recipe data from an HTML document.

---
+++ backend/middleware/parseRecipeData.js
@@ -0,0 +1,49 @@
+const cheerio = require("cheerio")
+const axios = require("axios")
+
+async function parseRecipeData(req, res, next){
+    const {url} = req.body
+
+    if(!url){
+        return res.status(400).json({error: "Please provide a URL."})
+    }
+
+    // Get the HTML document
+    const response = await axios.get(url);
+    const html = response.data;
+
+    // Use Cheerio to parse the HTML
+    const $ = cheerio.load(html);
+
+    let jsonld;
+    const node = $('script[type="application/ld+json"]').get(0);
+
+    // Try to read JSON
+    try {
+        jsonld = JSON.parse(node.firstChild.data);
+    } catch (err) {
+        return res.status(400).json({error: "Cannot read recipe. Please input recipe details manually."})
+    }
+    const array = jsonld["@graph"]
+    const recipeObj = array.filter(obj => obj['@type'] === 'Recipe')[0];
+    // console.log(recipeObj)
+
+    let recipe
+    // if the recipeObj exists, attach recipe to the request
+    if(recipeObj){
+        recipe = {
+            url, 
+            name: recipeObj.name, 
+            recipeIngredient: recipeObj.recipeIngredient,
+            recipeInstructions: recipeObj.recipeInstructions.map(instruction => instruction.text)
+        }
+    } else {
+        return res.status(400).json({error: "Cannot read recipe. Please input recipe details manually."})
+    }
+    
+    req.body = recipe
+    req.parsed = true    
+    next()
+}
+
+module.exports = parseRecipeData
