fc0a925df91521daf64ff7a1f38e91f959dde073
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-29 12:12:43 -0400

    Created Search.jsx component
    A form that takes ingredient state, error state, setIngredient, and handleSearch from props

---
+++ frontend/src/components/Search.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+export default function RecipeUrlForm(props) {
+    
+    return(
+        <form className="create" onSubmit={(e) => props.handleSearch(e)}>
+            <h3>Search recipes by ingredient</h3>
+            
+            <label>Ingredient name:</label>
+            <input 
+                type="text"
+                onChange={(e) => props.setIngredient(e.target.value)}
+                value={props.ingredient}
+            />
+
+            <button>Search</button>
+            {props.error && <div className="error">{props.error}</div>}
+        </form>
+    )
+}
+
+
