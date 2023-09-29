d683587357ee9b27101a8e602bc38ac020305638
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-09-29 12:13:36 -0400

    Update Home.jsx
    Added search component to home. Added ingredient and error state; added a handleSearch function that makes the fetch request to the search endpoint. These are passed down to the search component in props.

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,31 +1,65 @@
-import { useEffect, useContext } from "react"
+import { useState, useContext } from "react"
 import { Navigate } from "react-router-dom"
 import RecipeDetails from "../components/RecipeDetails"
 import RecipeManualForm from "../components/RecipeManualForm"
 import RecipeUrlForm from "../components/RecipeUrlForm"
+import Search from "../components/Search"
 import { RecipesContext } from "../context/RecipeContext"
 import { AuthContext } from "../context/AuthContext"
 
 export default function Home() {
+    const [ingredient, setIngredient] = useState("")
+    const [error, setError] = useState(null)
     const {recipes, dispatch: recipeDispatch} = useContext(RecipesContext)
     const {user} = useContext(AuthContext)
 
-    useEffect(()=>{
+    // useEffect(()=>{
         
-        async function fetchRecipes() {
-            const response = await fetch("/api/recipes")
-            const json = await response.json()
+    //     async function fetchRecipes() {
+    //         const response = await fetch("/api/recipes")
+    //         const json = await response.json()
 
-            if(response.ok){
-                recipeDispatch({type:"SET_RECIPES", payload: json})
+    //         if(response.ok){
+    //             recipeDispatch({type:"SET_RECIPES", payload: json})
+    //         }
+    //     }
+
+    //     if(user){
+    //         fetchRecipes()
+    //     }
+
+    // }, [user])
+
+    const handleSearch = async (e) =>{
+        e.preventDefault()
+
+        if (!user) {
+            setError("You must be logged in")
+            return
+        }
+
+        const response = await fetch("/api/recipes/search", {
+            method: "POST",
+            body: JSON.stringify({ ingredient }),
+            headers: {
+                "Content-Type": "application/json"
             }
+        })
+
+        const json = await response.json()
+        console.log(response)
+
+        if(!response.ok){
+            setError(json.error)
+        }
+        if(response.ok){
+            setIngredient("")
+            setError(null)
+            recipeDispatch({type:"SET_RECIPES", payload: json})
         }
 
-        if(user){
-            fetchRecipes()
-        }
 
-    }, [user])
+    }
 
     return (
         <>
@@ -40,6 +74,12 @@ export default function Home() {
                         ))}
                     </div>
                     <div>
+                        <Search
+                            ingredient={ingredient}
+                            setIngredient={setIngredient} 
+                            handleSearch={handleSearch} 
+                            error={error}
+                        />
                         <RecipeUrlForm/>
                         <RecipeManualForm />
                     </div>
